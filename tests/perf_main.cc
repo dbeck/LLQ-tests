140f5aa3fdd7a36809b3181cfa5a2800249e2444
David Beck <david.beck.priv@gmail.com>
2016-03-19 18:17:15 +0100

    more layout checks

--- tests/perf_main.cc
+++ tests/perf_main.cc
@@ -9,15 +9,39 @@ int main()
   #define MEASURE(F,N) LLQ::test::perf<N>(#F, F);
   
   MEASURE(baseline, 1000);
+  
   {
     char           b[4096];
-    LLQ::layout_1  ll1{b, sizeof(b)};
+    LLQ::layout_1  ll{b, sizeof(b)};
     
-    auto ll1_test = [&ll1]() {
-      ll1.add("hello",5);
+    auto ll1_nocheck_1b = [&ll]() {
+      ll.add_nocheck("hello",1);
     };
     
-    MEASURE(ll1_test, 100);
+    MEASURE(ll1_nocheck_1b, 100);
   }
+
+  {
+    char           b[4096];
+    LLQ::layout_1  ll{b, sizeof(b)};
+    
+    auto ll1_checked_5b = [&ll]() {
+      ll.add("hello",5);
+    };
+    
+    MEASURE(ll1_checked_5b, 100);
+  }
+  
+  {
+    char           b[4096];
+    LLQ::layout_1  ll{b, sizeof(b)};
+    
+    auto ll2_nocheck_5b = [&ll]() {
+      ll.add_nocheck("hello",5);
+    };
+    
+    MEASURE(ll2_nocheck_5b, 100);
+  }
+  
   return 0;
 }
